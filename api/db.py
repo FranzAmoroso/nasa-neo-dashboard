74e5643f1e150f822529ff33eb5c743ae09aedb0
Franz Amoroso <franz.amoroso@outlook.de>
2026-05-01 22:16:47 +0200

    added test for api in the frontend (page.js)

--- api/db.py
+++ api/db.py
@@ -21,19 +21,19 @@ async def get_cache(start_date: str, end_date: str):
         json_data = await r.get(cache_key)
         if json_data:
             return json.loads(json_data)
-        #print(f"DEBUG: Chiamo NASA con URL: {API_URL}")
+        print(f"DEBUG: Chiamo NASA con URL: {API_URL}")
         async with httpx.AsyncClient() as client:
             params = {
             "start_date": start_date,
             "end_date": end_date,
             "api_key":API_KEY
             }  
-            #print(f"DEBUG: Parametri: {params}")
+            print(f"DEBUG: Parametri: {params}")
         
             response = await client.get(API_URL, params= params, timeout = 50.0)
 
             if response.status_code != 200:
-                #print(f"NASA REJECTED: {response.status_code} - {response.text}")
+                print(f"NASA REJECTED: {response.status_code} - {response.text}")
                 raise HTTPException(status_code= response.status_code, detail= "Error API" )
         
             data = response.json()
@@ -41,5 +41,5 @@ async def get_cache(start_date: str, end_date: str):
         await r.set(cache_key, json.dumps(data), ex=604800)
         return data
     except Exception as e:
-        #print(f"ERRORE SERVER: {type(e).__name__} - {e}")
+        print(f"ERRORE SERVER: {type(e).__name__} - {e}")
         return None
