d0ba9789288d7fb0aa39c90e81eca9734abb94cb
Franz Amoroso <franz.amoroso@outlook.de>
2026-05-02 01:08:46 +0200

    path api change

--- frontend/api/index.py
+++ frontend/api/index.py
@@ -6,7 +6,7 @@ from control import convert_date
 app = FastAPI()
 
 
-@app.get("/asteroids/feed")
+@app.get("/api/asteroids/feed")
 
 async def get_asteroids(
     start_date: str = Query(..., pattern=r"^\d{2}-\d{2}-\d{4}$"),
