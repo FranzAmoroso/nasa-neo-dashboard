fd5b8a50378aec8b1c743a6a6b22ae90d18f214c
Franz Amoroso <franz.amoroso@outlook.de>
2026-05-02 00:54:16 +0200

    path api change

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
     async function fetchData() {
       try {
         const start = "10-04-2026";
-        const end = "16-04-2026";
+        const end = "11-04-2026";
         const url = `/api/asteroids/feed?start_date=${start}&end_date=${end}`;
         const response = await fetch(url);
         const result = await response.json();
