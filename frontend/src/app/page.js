6da001ace10c5092b005206047aa80e6f2915140
Franz Amoroso <franz.amoroso@outlook.de>
2026-05-01 22:26:30 +0200

    added test for api in the frontend (page.js)

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,4 +1,4 @@
-"use client"; 
+"use client";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -8,7 +8,13 @@ export default function Home() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch('/api/asteroids/feed?start_date=01-05-2024');
+        const start = "10-04-2026";
+        const end = "16-04-2026";
+
+        const response = await fetch(
+          `/api/asteroids/feed?start_date=${start}&end_date=${end}`,
+        );
+
         const result = await response.json();
         setData(result);
       } catch (error) {
