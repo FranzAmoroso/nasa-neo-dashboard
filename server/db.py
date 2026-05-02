3b7c702c2a9153713a2eb66abec751d9055ca941
Franz Amoroso <franz.amoroso@outlook.de>
2026-05-02 23:05:54 +0200

    deploy test

--- server/db.py
+++ server/db.py
@@ -1,4 +1,4 @@
-from redis.asyncio import Redis as redis_asyncio
+import redis
 import json
 import httpx
 import os
@@ -10,7 +10,7 @@ API_KEY = os.getenv("API_KEY")
 REDIS_URL = os.getenv("REDIS_URL")
 
 
-r = redis_asyncio.from_url(REDIS_URL)
+r = redis.Redis.from_url(REDIS_URL)
 async def get_cache(start_date: str, end_date: str):
     cache_key = f"nasa:{start_date}:{end_date}"
 
