b241515cbd1dcef464a42fe8fcce7928c42c6647
Franz Amoroso <franz.amoroso@outlook.de>
2026-05-03 17:18:58 +0200

    Added item (meteorite)

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Plus } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
