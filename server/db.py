5a98372f0ad0d85c11ca3ded9b357ea8dcebd088
Franz Amoroso <franz.amoroso@outlook.de>
2026-05-03 02:03:20 +0200

    Deploy test

--- server/db.py
+++ server/db.py
@@ -26,19 +26,17 @@ async def get_cache(start_date: str, end_date: str):
             "start_date": start_date,
             "end_date": end_date,
             "api_key":API_KEY
-            }  
-            print(f"DEBUG: Parametri: {params}")
-        
+            }
+            print("CACHE MISS → chiamo NASA")
             response = await client.get(API_URL, params= params, timeout = 50.0)
 
             if response.status_code != 200:
-                print(f"NASA REJECTED: {response.status_code} - {response.text}")
                 raise HTTPException(status_code= response.status_code, detail= "Error API" )
         
             data = response.json()
         
         await r.set(cache_key, json.dumps(data), ex=604800)
+        print("SALVO IN REDIS:", cache_key)
         return data
     except Exception as e:
-        print(f"ERRORE SERVER: {type(e).__name__} - {e}")
         raise HTTPException(status_code=500, detail=str(e))
