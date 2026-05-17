65b03c48f84dd34873cb4ec484137d351007b88e
Franz Amoroso <franz.amoroso@outlook.de>
2026-05-17 18:48:59 +0200

    Cross origin update

--- server/main.py
+++ server/main.py
@@ -13,7 +13,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, 
+    allow_origins="https://nasa-neo-dashboard-franzamorosos-projects.vercel.app", 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
